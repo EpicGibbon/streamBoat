113f79c5d0a05ac245a702c420249cae06118838
EpicGibbon <michant1997@gmail.com>
2020-08-15 21:29:38 -0400

    initial carousel

--- client/src/pages/Discover/index.js
+++ client/src/pages/Discover/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Hero from '../../components/Hero';
+import CarouselTile from '../../components/CarouselTile'
+import Slider from "react-slick";
 
 function Discover(props) {
     return(
         <div>
             <Hero title={props.title} />
+            <CarouselTile />
             <p>
                 Discover page works
             </p>
