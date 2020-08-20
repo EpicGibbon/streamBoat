f3e1056b14e131ee94f2cc13fb102a97b4bf4cf0
EpicGibbon <michant1997@gmail.com>
2020-08-20 12:04:31 -0400

    initial routes for clientside

--- client/src/pages/BandPage/index.js
+++ client/src/pages/BandPage/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import BandHeader from '../../components/BandHeaderImg'
-import BandMerch from '../../components/BandMerch'
 import AboutMe from '../../components/AboutMe'
-import Row from 'react-bootstrap/Row'
-
 
 class BandPage extends React.Component {
     render() {
         return (
             <div>
                 <BandHeader />
-                <Row>
-                    <BandMerch md={3} style={{display: 'inline-block'}}/>
-                </Row>
                 <AboutMe />
             </div>
         )
